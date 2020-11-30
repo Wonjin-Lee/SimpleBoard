65df1f8b32aea95459a1606381601586244a0e33
Wonjin Lee <claude17@naver.com>
2020-11-30 22:10:53 +0900

    기본 설정 완료된 프로젝트 파일 커밋

---
+++ src/main/java/com/spacewhale/board/mapper/BoardMapper.java
@@ -0,0 +1,11 @@
+package com.spacewhale.board.mapper;
+
+import com.spacewhale.board.dto.BoardDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BoardMapper {
+    List<BoardDto> selectBoardList() throws Exception;
+}
