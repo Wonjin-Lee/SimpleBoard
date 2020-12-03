bea3aaddde735ca706f6f59e71bd5f1e71ade617
Wonjin Lee <claude17@naver.com>
2020-12-03 23:02:11 +0900

    [기능] Logback 관련 설정 파일 추가

--- src/main/java/com/spacewhale/board/controller/BoardController.java
+++ src/main/java/com/spacewhale/board/controller/BoardController.java
@@ -2,6 +2,8 @@ package com.spacewhale.board.controller;
 
 import com.spacewhale.board.dto.BoardDto;
 import com.spacewhale.board.service.BoardService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +15,15 @@ import java.util.List;
 @Controller
 public class BoardController {
 
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private BoardService boardService;
 
     @RequestMapping("/board/openBoardList.do")
     public ModelAndView openBoardList() throws Exception {
+        log.debug("openBoardList");
+
         ModelAndView mv = new ModelAndView("/board/boardList");
 
         List<BoardDto> list = boardService.selectBoardList();
