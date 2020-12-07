44bfeec7e5b488ee7252944a2fd287cf82c3525e
Wonjin Lee <claude17@naver.com>
2020-12-07 22:35:13 +0900

    [기능] 인터셉터를 이용하여 컨트롤러 호출 전, 후의 로그 출력 기능 추가

---
+++ src/main/java/com/spacewhale/board/configuration/WebMvcConfiguration.java
@@ -0,0 +1,14 @@
+package com.spacewhale.board.configuration;
+
+import com.spacewhale.board.interceptor.LoggerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new LoggerInterceptor());
+    }
+}
