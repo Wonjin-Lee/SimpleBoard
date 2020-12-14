36db16501132dee4633c22407181ba58582efb12
Wonjin Lee <claude17@naver.com>
2020-12-14 22:03:26 +0900

    [기능] 파일을 업로드하기 위한 스프링 빈 설정(Apache Common Fileupload)
    + multipartResolver 빈을 등록했기 때문에 첨부파일 관련 자동 구성을 사용하지 않도록 변경

--- src/main/java/com/spacewhale/board/configuration/WebMvcConfiguration.java
+++ src/main/java/com/spacewhale/board/configuration/WebMvcConfiguration.java
@@ -1,14 +1,27 @@
 package com.spacewhale.board.configuration;
 
 import com.spacewhale.board.interceptor.LoggerInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
+
     @Override
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(new LoggerInterceptor());
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+
+        commonsMultipartResolver.setDefaultEncoding("UTF-8");
+        commonsMultipartResolver.setMaxUploadSizePerFile(5 * 1024 * 1024); // max size : 5mb
+
+        return commonsMultipartResolver;
     }
 }
