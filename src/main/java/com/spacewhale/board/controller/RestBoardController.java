6dc3b8ea8ac6a2f62bc2b112b589651dd737d9cb
Wonjin Lee <claude17@naver.com>
2020-12-28 00:20:56 +0900

    [기능] RESTful API를 제공하기 위해 대체할 Controller 개발

---
+++ src/main/java/com/spacewhale/board/controller/RestBoardController.java
@@ -0,0 +1,100 @@
+package com.spacewhale.board.controller;
+
+import com.spacewhale.board.dto.BoardDto;
+import com.spacewhale.board.dto.BoardFileDto;
+import com.spacewhale.board.service.BoardService;
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.net.URLEncoder;
+import java.util.List;
+
+/**
+ * RESTful URI
+ * 게시판 목록           /board          GET
+ * 게시글 작성 화면       /board/write    GET
+ * 게시글 작성           /board/write    POST
+ * 게시글 상세 화면       /board/글번호    GET
+ * 게시글 수정           /board/글번호    PUT
+ * 게시글 삭제           /board/글번호    DELETE
+ * 첨부파일 다운로드      /board/file     GET
+ */
+
+public class RestBoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @RequestMapping(value = "/board", method = RequestMethod.GET)
+    public ModelAndView openBoardList() throws Exception {
+        ModelAndView mv = new ModelAndView("/board/restBoardList");
+
+        List<BoardDto> list = boardService.selectBoardList();
+        mv.addObject("list", list);
+
+        return mv;
+    }
+
+    @RequestMapping(value = "/board/write", method = RequestMethod.GET)
+    public String openBoardWrite() throws Exception {
+        return "/board/restBoardWrite";
+    }
+
+    @RequestMapping(value = "/board/write", method = RequestMethod.POST)
+    public String insertBoard(BoardDto boardDto, MultipartHttpServletRequest multipartHttpServletRequest) throws Exception {
+        boardService.insertBoard(boardDto, multipartHttpServletRequest);
+        return "redirect:/board";
+    }
+
+    @RequestMapping(value = "/board/{boardIdx}", method = RequestMethod.GET)
+    public ModelAndView openBoardDetail(@PathVariable("boardIdx") int boardIdx) throws Exception {
+        ModelAndView mv = new ModelAndView("/board/restBoardDetail");
+
+        BoardDto boardDto = boardService.selectBoardDetail(boardIdx);
+        mv.addObject("board", boardDto);
+
+        return mv;
+    }
+
+    @RequestMapping(value = "/board/{boardIdx}", method = RequestMethod.PUT)
+    public String updateBoard(BoardDto boardDto) throws Exception {
+        boardService.updateBoard(boardDto);
+        return "redirect:/board";
+    }
+
+    @RequestMapping(value = "/board/{boardIdx}", method = RequestMethod.DELETE)
+    public String deleteBoard(@PathVariable("boardIdx") int boardIdx) throws Exception {
+        boardService.deleteBoard(boardIdx);
+        return "redirect:/board";
+    }
+
+    @RequestMapping(value = "/board/file", method = RequestMethod.GET)
+    public void downloadBoardFile(@RequestParam int idx, @RequestParam int boardIdx, HttpServletResponse response) throws Exception {
+        BoardFileDto boardFileDto = boardService.selectBoardFileInformation(idx, boardIdx);
+
+        if (ObjectUtils.isEmpty(boardFileDto) == false) {
+            String fileName = boardFileDto.getOriginalFileName();
+
+            byte[] files = FileUtils.readFileToByteArray(new File(boardFileDto.getStoredFilePath()));
+
+            response.setContentType("application/octet-stream");
+            response.setContentLength(files.length);
+            response.setHeader("Content-Disposition", "attachment; filename=\""
+                    + URLEncoder.encode(fileName, "UTF-8") + "\";");
+            response.setHeader("Content-Transfer-Encoding", "binary");
+
+            response.getOutputStream().write(files);
+            response.getOutputStream().flush();
+            response.getOutputStream().close();
+        }
+    }
+}
